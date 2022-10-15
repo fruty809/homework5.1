93e4a1028919ceca8dd88f91d4aad717e5bd40be
fruty809 <115156929+fruty809@users.noreply.github.com>
2022-10-15 10:52:15 +0600

    Update Boss.java

--- src/Boss.java
+++ src/Boss.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Boss {
+public class Hero {
+    int damage;
+    int health;
+    String ultimateSkill;
+
+    public int getHealth() {
+        return this.health;
+    }
+
+    public int getDamage() {
+        return this.damage;
+    }
+
+    public String getUltimateSkill(){
+        return this.ultimateSkill;
+    }
+    public Hero(int theDamage, int theHealth, String theUltimateSkill){
+        damage = theDamage;
+        health = theHealth;
+        ultimateSkill = theUltimateSkill;
+    }
+    public Hero(int damage, int health){
+        this.damage = damage;
+        this.health = health;
+    }
 }
